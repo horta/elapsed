1702564882f0438a859528635572a62a1a12c4e9
Danilo Horta <danilo.horta@pm.me>
2021-09-23 11:34:59 +0100

    check for noreturn

--- src/fatal.h
+++ src/fatal.h
@@ -3,8 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef HAVE_STDNORETURN_H
+#include <stdnoreturn.h>
+#endif
 
-_Noreturn static inline void fatal(char const *msg)
+#ifndef noreturn
+#define noreturn
+#endif
+
+noreturn static inline void fatal(char const *msg)
 {
     fprintf(stderr, "%s", msg);
     fflush(stderr);
