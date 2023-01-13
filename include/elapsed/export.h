f8799249ad9e8f8e2f6972b1139cc76588887f8d
Danilo Horta <danilo.horta@pm.me>
2023-01-13 12:29:04 +0000

    export.h

--- include/elapsed/export.h
+++ include/elapsed/export.h
@@ -1,6 +1,24 @@
 #ifndef ELAPSED_API_H
 #define ELAPSED_API_H
 
-#define ELAPSED_API __attribute__((visibility("default")))
+/* clang-format off */
+#ifdef elapsed_STATIC_DEFINE
+#  define ELAPSED_API
+#else
+#  ifdef elapsed_EXPORTS /* We are building this library */
+#    ifdef _WIN32
+#      define ELAPSED_API __declspec(dllexport)
+#    else
+#      define ELAPSED_API __attribute__((visibility("default")))
+#    endif
+#  else /* We are using this library */
+#    ifdef _WIN32
+#      define ELAPSED_API __declspec(dllimport)
+#    else
+#      define ELAPSED_API __attribute__((visibility("default")))
+#    endif
+#  endif
+#endif
+/* clang-format on */
 
 #endif
